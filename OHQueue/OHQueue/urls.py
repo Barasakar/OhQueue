18017edefe4b016cfa61390f51ff3a820e18e4ef
barasakar <jlinpoughkeepsieday@gmail.com>
2023-11-10 14:11:06 -0600

    set up Django locally. Started working on login page

--- OHQueue/OHQueue/urls.py
+++ OHQueue/OHQueue/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from queue_app import views as queue_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("queue_app/", include("queue_app.urls")),
+    path("", queue_views.renderLoginPage, name="login_page"), # this assigns the login page to the root link.
 ]
--- OHQueue/queue_app/views.py
+++ OHQueue/queue_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def renderLoginPage(request):
+    return render(request, "queue_app/login_page.html")
